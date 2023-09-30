65e8340f1bb68f9317911d0ec5ce7e27d10c3d4c
Erick Cunha <122950360+erickcunha1@users.noreply.github.com>
2023-09-30 20:13:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from whatsbot import WhatsAppBot
+import WhatsAppClass as ws
 
 
 excel_path = 'contatos.xlsx'
 user_data_directory = '/Users/user/Library/Application Support/Google/Chrome/Selenium' 
-bot = WhatsAppBot(user_data_directory, excel_path)
+bot = ws.WhatsAppBot(user_data_directory, excel_path)
 bot.send_messages()
 bot.close()
